2d7b42834d85c27aa25b9bb3fc04fcfd04d37b4b
Revolist <64810943+revolist@users.noreply.github.com>
2021-03-18 10:48:54 -0300

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Grid from "./Grid.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+    height: calc(80vh);
+  }
+</style>
+
+<main>
+	<Grid />
+</main>
